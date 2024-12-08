d2f26204c3e5fed2c6cf9fa5c0388bf08c922935
User <user@Users-MacBook-Pro.local>
2024-12-08 15:53:56 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1> Rosella Echeruo</h1>
+      <p>Hi My Name Is Rosella Echeruo, a 27 years old female gradute of unilag, married presently studying frontend development at Gomycode institute. below are few things about myself.
+
+</p>
+<p>My Favorite Foods are: Shrimp and lobster, Fried rice and salad, Chicken and chips, Spagetti and turkey, Catfish barbecue, and Shawarma</p>
+<p>My Faviourite Movies are: The bee keeper, Wrath of man, Acrimony, Anikulapo, Fabricated city, and Sisu</p>
+
+<p>Items  i like when eating out are: Coffee: A hot black drink that i enjoy in the morning.
+Smothie: a blended mixture of fruits, vegetables, liquid, and sometimes other ingredients such as nuts, seeds, oats, yogurt, and ice, which i like taking in a hot aftternoon.
+Tea: A warm drink that helps me relax.
+Juice: A cold refreshing drinks made from fruits.
+Cocktail: a combination of one or more spirits mixed with other ingredients, such as juices, flavored syrups, tonic water that gives a tipsy feeling.</p>
+<p> My Favorite Company/brands are: Apple Inc, Telsa, Mercedes-Benz</p>
+
+
+
+
+    </>
+  )
+}
+
+export default App
